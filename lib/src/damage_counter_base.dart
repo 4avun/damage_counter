637289078ae68854aaaa73411adfdbd3bb263a22
4avun <savaoffgd@gmail.com>
2023-12-05 17:21:08 +0200

    second

--- lib/src/damage_counter_base.dart
+++ lib/src/damage_counter_base.dart
@@ -4,7 +4,7 @@ import 'package:damage_counter/src/damage_counter_node.dart';
 class DamageCounter {
   void update(List<DamageCounterAction> actions) {
     for (var action in actions) {
-      
+      print(action);
     }
   }
 }
